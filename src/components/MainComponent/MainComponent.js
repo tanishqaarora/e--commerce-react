5a0066d9d8f98d5b6fbe5692631c4255e962c211
Tanishqa Arora <tanishqa164@gmail.com>
2023-08-09 17:32:46 +0530

    changes in main component

--- src/components/MainComponent/MainComponent.js
+++ src/components/MainComponent/MainComponent.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import Home from '../Home/Home';
+import { Routes, Route } from 'react-router-dom';
+import Layout from '../Layout/Layout';
+import Register from '../Auth/Register';
+import Login from '../Auth/Login';
 
 const MainComponent = () => {
     return (
-        <div>
-            <Home/>
-        </div>
+        <>
+            <div>
+                <Layout>
+                    <Routes>
+                        <Route path='/' element={ <Home/> }/>
+                        <Route path='/register' element={ <Register/> }/>
+                        <Route path='/login' element={ <Login/> }/>
+                    </Routes>
+                </Layout>
+            </div>
+        </>
     )
 }
 
